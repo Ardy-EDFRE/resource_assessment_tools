3c8798d10086f140702d096b1cdb062f3150da08
Ardeshir Beheshti <ardeshir.beheshti@edf-re.com>
2022-06-08 15:38:04 -0700

    About me page creation

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ from PIL import  Image
 
 # Custom imports 
 from multipage import MultiPage
-from pages import iec_v2, dnv_solar, sector # import your pages here
+from pages import iec_v2, dnv_solar, about_me # import your pages here
 
 # Create an instance of the app 
 app = MultiPage()
@@ -20,6 +20,7 @@ col1.image(display, width = 400)
 col2.title("Resource Assessment Tools")
 
 # Add all your application here
+app.add_page("About the page", about_me.app)
 app.add_page("IEC Terrain Assessment", iec_v2.app)
 app.add_page("DNV Solar Energy Estimate", dnv_solar.app)
 # The main app
---
+++ pages/about_me.py
@@ -0,0 +1,78 @@
+import streamlit as st
+
+def app():
+    st.markdown("""<!-- Add logo -->
+                <!--  ![App Logo]([[https://i.stack.imgur.com/ARgpq.jpg](https://github.com/Ardy-EDFRE/resource_assessment_tools/blob/main/edf_logo.jpg](https://raw.githubusercontent.com/Ardy-EDFRE/resource_assessment_tools/main/edf_logo.jpg))) -->
+                
+                ![data-storyteller](https://raw.githubusercontent.com/Ardy-EDFRE/resource_assessment_tools/main/edf_logo.jpg)
+                
+                [![forthebadge](https://forthebadge.com/images/badges/built-by-developers.svg)](https://forthebadge.com)
+                [![pythonbadge](https://forthebadge.com/images/badges/made-with-python.svg)](https://forthebadge.com)
+                [![Streamlit App](https://static.streamlit.io/badges/streamlit_badge_black_white.svg)](https://share.streamlit.io/ardy-edfre/resource_assessment_tools/main/app.py)
+                
+                # 📱 Resource Assessment Tools 📉
+                
+                _**ONE STOP SOLUTION FOR ALL YOUR DATA NEEDS**_ 
+                ## Introduction 
+                
+                ## Tool Description 
+                A collection of tools that can be used by Wind &amp; Solar teams to analyze specific resource assessments.
+                
+                ## Features 
+                
+                ## 📝 Module-Wise Description
+                
+                The application also uses Streamlit for a multiclass page implementation which can be viewed in the `multipage.py` file. The UI of the application can be seen here. The application is divided into multiple modules, each of which have been described below.
+                
+                ![UI of the application](https://i.stack.imgur.com/MOVpz.png)
+                
+                
+                _📌 **IEC Terrain Assessment**_ <br/>
+                
+                 
+                
+                _📌 **DNV Solar Energy Estimate**_ <br/>
+                
+                
+                ## Technology Stack 
+                
+                1. Python 
+                2. Streamlit 
+                
+                # How to Run 
+                
+                - Clone the repository
+                - Setup Virtual environment
+                ```
+                $ python3 -m venv env
+                ```
+                - Activate the virtual environment
+                ```
+                $ source env/bin/activate
+                ```
+                - Install dependencies using
+                ```
+                $ pip install -r requirements.txt
+                ```
+                - Run Streamlit
+                ```
+                $ streamlit run app.py
+                ```
+                
+                ## Other Content
+                
+                ## 🤝 How to Contribute? [3]
+                
+                - Take a look at the Existing Issues or create your own Issues!
+                - Wait for the Issue to be assigned to you after which you can start working on it.
+                - Fork the Repo and create a Branch for any Issue that you are working upon.
+                - Create a Pull Request which will be promptly reviewed and suggestions would be added to improve it.
+                - Add Screenshots to help us know what this Script is all about.
+                
+                
+                # 👨‍💻 Contributors ✨
+                
+                ## Contact
+                
+                For any feedback or queries, please reach out to [ardeshir.beheshti@edf-re.com](ardeshir.beheshti@edf-re.com).
+                """)
