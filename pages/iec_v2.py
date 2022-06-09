dd8be18927514f270c41ce6f0a96178f61376ac9
Ardeshir Beheshti <ardeshir.beheshti@edf-re.com>
2022-06-09 10:15:30 -0700

    fixing map display on IEC

--- pages/iec_v2.py
+++ pages/iec_v2.py
@@ -771,10 +771,11 @@ def app():
                 'CRS {} '.format(
                     turbine_CRSCheck.crs, raster_CRSCheck.crs))
 
+    st.dataframe(turbine_CRSCheck)
+
     display_iec_map = st.sidebar.checkbox("Display Turbine Layout on Map")
     if display_iec_map:
         turbine_CRSCheck = turbine_CRSCheck.to_crs("EPSG:4326")
-        st.dataframe(turbine_CRSCheck)
         turbines_df = turbine_CRSCheck.iloc[:, [turbine_CRSCheck.columns.get_loc(c) for c in ['target_turbine_fid', 'target_turbine_x', 'target_turbine_y']]]
         mets_df = turbine_CRSCheck.iloc[:, [turbine_CRSCheck.columns.get_loc(c) for c in ['target_met_fid', 'target_met_x', 'target_met_y']]]
         turbines_df["long"] = turbines_df.geometry.x
