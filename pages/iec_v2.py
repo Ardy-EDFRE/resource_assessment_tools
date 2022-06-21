2b98a7ccade75a3259f40992cab452fd622393a9
Ardeshir Beheshti <ardeshir.beheshti@edf-re.com>
2022-06-21 15:35:50 -0700

    Testing to see if results are showing for sector results

--- pages/iec_v2.py
+++ pages/iec_v2.py
@@ -891,16 +891,22 @@ def app():
                           popup='Met',
                           icon=met_icon).add_to(mets_cluster)
 
-        bounding_box = turbines_cluster.get_bounds()
-        turbine_map.fit_bounds([bounding_box])
-        folium_static(turbine_map, width=800, height=800)
-
         sectors_df = {'geometry': paired_results}
         sectors_gdf = geopandas.GeoDataFrame(sectors_df, geometry='geometry', crs=4326)
 
         sectors_geojson = sectors_gdf.to_json()
-        folium.Choropleth(sectors_geojson,
-                          data=sectors_geojson).add_to(turbine_map)
+        folium.Choropleth(geo_data=sectors_geojson,
+                          data=sectors_geojson,
+                          fill_color="YlGn",
+                          fill_opacity=0.7,
+                          line_opacity=.1,
+                          ).add_to(turbine_map)
+
+        folium.LayerControl().add_to(turbine_map)
+
+        bounding_box = turbines_cluster.get_bounds()
+        turbine_map.fit_bounds([bounding_box])
+        folium_static(turbine_map, width=800, height=800)
 
     run_iec = st.sidebar.button("Run IEC Terrain Assessment",
                                 help="This will run the process for evaluation sectors and generate an output for display & download")
