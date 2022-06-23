9a25be32629b461c608728c95037d4ab63a202fd
Ardeshir Beheshti <ardeshir.beheshti@edf-re.com>
2022-06-23 15:09:56 -0700

    Testing to see if results are showing for sector results

--- pages/iec_v2.py
+++ pages/iec_v2.py
@@ -899,9 +899,7 @@ def app():
 
         sectors_df = pd.DataFrame({'Geometry': paired_results})
 
-        sectors_gdf = geopandas.GeoDataFrame(sectors_df, geometry='Geometry')
-        sectors_gdf['wkt'] = sectors_gdf.geometry.to_wkt()
-        sectors_gdf.geometry.map(lambda polygon: shapely.ops.transform(lambda y, x: (x, y), shapely.wkt.loads(polygon)))
+        sectors_gdf = geopandas.GeoDataFrame(sectors_df, geometry=sectors_df['Geometry'].apply(shapely.wkt.loads))
 
         sectors_gdf.set_crs(epsg=4326, inplace=True)
         sectors_gdf = sectors_gdf.to_crs("EPSG:4326")
