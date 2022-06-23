4b4ccf32a02d30840657b4987dc5d4cf5e6f3f81
Ardeshir Beheshti <ardeshir.beheshti@edf-re.com>
2022-06-23 15:33:37 -0700

    Testing to see if results are showing for sector results

--- pages/iec_v2.py
+++ pages/iec_v2.py
@@ -900,13 +900,13 @@ def app():
         sectors_df = pd.DataFrame({'Geometry': paired_results})
 
         sectors_gdf = geopandas.GeoDataFrame(sectors_df, geometry='Geometry')
-        sectors_gdf.geometry.map(lambda sectors_gdf: shapely.ops.transform(lambda y, x: (x, y), shapely.wkt.loads(sectors_gdf)))
 
         sectors_gdf.set_crs(epsg=4326, inplace=True)
         sectors_gdf = sectors_gdf.to_crs("EPSG:4326")
 
         x1, y1, x2, y2 = sectors_gdf['Geometry'].total_bounds
-        sectors_map.fit_bounds([y1, x1], [y2, x2])
+        sectors_map.fit_bounds([[y1, x1], [y2, x2]])
+        folium.GeoJson(sectors_gdf['Geometry']).add_to(sectors_map)
         folium_static(sectors_map, width=800, height=800)
 
         bounding_box = turbines_cluster.get_bounds()
