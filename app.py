172df3a874704b4087aa2947ac1761baf015fa2e
Ardeshir Beheshti <63427542+Ardy-EDFRE@users.noreply.github.com>
2023-01-05 10:26:50 -0800

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 # Custom imports
 from multipage import MultiPage
 # import your pages here
-from pages import iec_v2, dnv_solar, about_me, solar_comparisons
+from pages import iec_v2, table_edits_aggrid, dnv_solar, about_me, solar_comparisons
 
 import streamlit as st
 import matplotlib.pyplot as plt
@@ -17,5 +17,6 @@ app.add_page("About the page", about_me.app)
 app.add_page("IEC Terrain Assessment", iec_v2.app)
 app.add_page("DNV Solar Energy Estimate", dnv_solar.app)
 app.add_page("Solar Estimate Comparisons", solar_comparisons.app)
+app.add_page("Aggrid Example", table_edits_aggrid.app)
 # The main app
 app.run()
